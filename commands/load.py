c5a90655380b2f9a8469f7357ee5f7f73e9bee58
HayderChammakhi <4101192@keranna.qc.ca>
2024-01-17 18:10:38 -0500

    Added intake subsystem

---
+++ commands/load.py
@@ -0,0 +1,28 @@
+import wpilib
+from utils.safecommand import SafeCommand
+from subsystems.intake import Intake
+from utils.property import autoproperty
+
+
+class Load(SafeCommand):
+    intake_time = autoproperty(1)
+
+    def __init__(self, intake: Intake):
+        super().__init__()
+        self.addRequirements(Intake)
+        self.intake = intake
+        self.timer = wpilib.Timer()
+
+    def initialize(self) -> None:
+        self.timer.reset()
+        self.timer.start()
+
+    def execute(self) -> None:
+        self.intake.takeIn()
+
+    def isFinished(self) -> bool:
+        return self.timer.get() >= self.open_time
+
+    def end(self, interrupted: bool) -> None:
+        self.timer.stop()
+        self.intake.stop()
--- subsystems/intake.py
+++ subsystems/intake.py
@@ -20,5 +20,5 @@ class Intake(SafeSubsystem):
     def stop(self):
         self.motor.stopMotor()
 
-    def takeIn(self):
+    def takeOut(self):
         self.motor.set(self.speed_out)
