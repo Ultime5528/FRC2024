80d8e15faa4504a503384d3fd1f637ad4ceb9ea0
Felix Campeau <4000329@keranna.qc.ca>
2024-01-22 18:11:57 -0500

    Separated climber in climberright and climberleft

--- subsystems/climber/climberright.py
+++ subsystems/climber/climberright.py
@@ -7,26 +7,21 @@ from utils.safesubsystem import SafeSubsystem
 from utils.sparkmaxutils import configureLeader
 
 
-class Climber(SafeSubsystem):
+class ClimberRight(SafeSubsystem):
     climber_speed_up = autoproperty(0.25)
     climber_speed_down = autoproperty(-0.25)
 
     def __init__(self):
         super().__init__()
-        self.climb_motor_left = rev.CANSparkMax(ports.climber_motor_left,
+        self.climb_motor_left = rev.CANSparkMax(ports.climber_motor_right,
                                                 rev.CANSparkMax.MotorType.kBrushless)
         configureLeader(self.climb_motor_left, "brake", False)
-        self.climb_motor_right = rev.CANSparkMax(ports.climber_motor_right,
-                                                 rev.CANSparkMax.MotorType.kBrushless)
-        configureLeader(self.climb_motor_right, "brake", False)
+
     def extend(self):
         self.climb_motor_left.set(self.climber_speed_up)
-        self.climb_motor_right.set(self.climber_speed_up)
 
     def retract(self):
         self.climb_motor_left.set(self.climber_speed_down)
-        self.climb_motor_right.set(self.climber_speed_down)
 
     def stop(self):
         self.climb_motor_left.set(0)
-        self.climb_motor_right.set(0)
