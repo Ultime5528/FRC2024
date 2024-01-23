7e8c0d25a2632d530f9812dc3d3dd1f7e9cfbed5
Étienne Thibault <4001491@keranna.qc.ca>
2024-01-22 20:04:28 -0500

    Shooter subsystem unit tested

--- subsystems/shooter.py
+++ subsystems/shooter.py
@@ -12,34 +12,26 @@ from utils.property import autoproperty
 class Shooter(SafeSubsystem):
     shooter_high_speed = autoproperty(0.5)
     shooter_low_speed = autoproperty(0.25)
-    shooter_angle_up_speed = autoproperty(0.2)
-    shooter_angle_down_speed = autoproperty(-0.2)
 
     def __init__(self):
         super().__init__()
 
         self.left_motor = rev.CANSparkMax(ports.shooter_motor_left, rev.CANSparkMax.MotorType.kBrushless)
-        configureLeader(self.left_motor, "brake")
+        configureLeader(self.left_motor, "coast")
 
         self.right_motor = rev.CANSparkMax(ports.shooter_motor_right, rev.CANSparkMax.MotorType.kBrushless)
-        configureFollower(self.right_motor, self.left_motor, "brake", inverted=True)
-
-        self.pivot_motor = wpilib.VictorSP(ports.shooter_pivot_motor)
-        self.pivot_encoder = self.pivot_motor.getEncoder()
+        configureFollower(self.right_motor, self.left_motor, "coast", inverted=True)
 
         if RobotBase.isSimulation():
             self.left_motor_sim = SparkMaxSim(self.left_motor)
             self.right_motor_sim = SparkMaxSim(self.right_motor)
-            self.pivot_motor_sim = SparkMaxSim(self.pivot_motor)
-
-    def angleUp(self):
-        self.pivot_motor.set(autoproperty(self.shooter_angle_up_speed))
-
-    def angleDown(self):
-        self.pivot_motor.set(autoproperty(self.shooter_angle_down_speed))
 
     def shootHigh(self):
         self.left_motor.set(self.shooter_high_speed)
 
     def shootLow(self):
         self.left_motor.set(self.shooter_low_speed)
+
+    def simulationPeriodic(self):
+        self.left_motor_sim.setVelocity(self.left_motor.get())
+        self.right_motor_sim.setVelocity(-self.left_motor.get())
--- tests/shooter_test.py
+++ tests/shooter_test.py
@@ -1,9 +1,4 @@
-from pyfrc.tests import *
-
-import wpilib.simulation
 import pytest
-from utils.sparkmaxsim import SparkMaxSim
-from subsystems.shooter import Shooter
 
 
 def test_ShootHigh(control, robot):
@@ -22,19 +17,3 @@ def test_ShootLow(control, robot):
         control.step_timing(seconds=0.1, autonomous=False, enabled=True)
         assert robot.shooter.shooter_low_speed == pytest.approx(robot.shooter.left_motor_sim.getVelocity(), rel=0.01)
         assert -1*robot.shooter.shooter_low_speed == pytest.approx(robot.shooter.right_motor_sim.getVelocity(), rel=0.01)
-
-
-# def test_AngleUp(control, robot):
-
-#     with control.run_robot():
-#         robot.shooter.AngleUp()
-#         control.step_timing(seconds=0.1, autonomous=False, enabled=True)
-#         assert robot.shooter.shooter_angle_up_speed == pytest.approx(robot.shooter.pivot_motor_sim.getVelocity(), rel=0.01)#
-
-
-# def test_AngleDown(control, robot):
-
-#     with control.run_robot():
-#         robot.shooter.AngleDown()
-#         control.step_timing(seconds=0.1, autonomous=False, enabled=True)
-#         assert robot.shooter.shooter_angle_down_speed == pytest.approx(robot.shooter.pivot_motor_sim.getVelocity(), rel=0.01)
