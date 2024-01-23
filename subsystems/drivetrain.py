3ba9d59b5f247cc307b93d1b9474bd7de151a0e2
Thomas McDonald <4001137@keranna.qc.ca>
2024-01-22 20:07:09 -0500

    Added beginning of alignwithtag2d

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -1,7 +1,9 @@
 import math
 
 import wpilib
-from wpilib import RobotBase
+from photonlibpy.photonCamera import PhotonCamera
+from photonlibpy.photonPipelineResult import PhotonPipelineResult
+from wpilib import RobotBase, RobotController
 from wpimath.estimator import SwerveDrive4PoseEstimator
 from wpimath.geometry import Pose2d, Translation2d, Rotation2d, Twist2d
 from wpimath.kinematics import (
@@ -26,6 +28,8 @@ class Drivetrain(SafeSubsystem):
     angular_offset_fr = autoproperty(0.0)
     angular_offset_bl = autoproperty(3.14)
     angular_offset_br = autoproperty(1.57)
+
+    use_vision = autoproperty(True)
 
     acceptable_wheel_rotation = autoproperty(0.51)  # is radians. Tolerance in which the wheel can be in
     wheel_flip_rotation = autoproperty(0.85)  # wheel will lock and flip
@@ -91,7 +95,9 @@ class Drivetrain(SafeSubsystem):
             Pose2d(0, 0, 0),
         )
 
-        if RobotBase.isSimulation():
+        if RobotBase.isReal():
+            self.cam = PhotonCamera("mainCamera")
+        else:
             self.sim_yaw = 0
 
     def drive(
