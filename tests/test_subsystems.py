afa3cb45dbeff988e7df970989f40f1bd192e459
LateAlways <javiparedes08@outlook.com>
2024-01-27 15:31:54 -0500

    Added tests for commands and subsystems

---
+++ tests/test_subsystems.py
@@ -0,0 +1,20 @@
+import inspect
+from pathlib import Path
+
+from commands2 import Subsystem
+
+from utils.safesubsystem import SafeSubsystem
+
+
+def get_subsystems():
+    subsystems = []
+    for file in Path("../subsystems").rglob("*.py"):
+        module = __import__(str(file).replace("..\\", "").replace("\\", ".").replace(".py", ""), fromlist=[""])
+        for name, obj in inspect.getmembers(module, inspect.isclass):
+            if issubclass(obj, Subsystem) and obj.__name__ != "SafeSubsystem":
+                subsystems.append(obj)
+    return subsystems
+
+def test_inheritance():
+    for obj in get_subsystems():
+        assert issubclass(obj, SafeSubsystem), f"{obj.__name__} is not a subclass of SafeSubsystem"
