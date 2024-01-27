36553c7f9f96031c8cbc85e9894fce318910bfc2
Felix Campeau <4000329@keranna.qc.ca>
2024-01-27 11:03:14 -0500

    Finishing touches RetractClimber and MoveClimber commands

--- commands/retractclimber.py
+++ commands/retractclimber.py
@@ -6,12 +6,13 @@ class RetractClimber(SafeCommand):
     def __init__(self, climber: Climber):
         super().__init__()
         self.climber = climber
+        self.addRequirements(climber)
 
     def execute(self):
         self.climber.retract()
 
     def isFinished(self) -> bool:
-        self.climber.isDown()
+        return self.climber.isDown()
 
     def end(self, interrupted: bool):
         self.climber.stop()
