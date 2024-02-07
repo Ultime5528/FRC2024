c28b77053a01b010e74c1e4b560d123a5c482bec
Felix Campeau <4000329@keranna.qc.ca>
2024-02-07 17:53:00 -0500

    Added periodic for reset and getHeight using offset

--- subsystems/climber.py
+++ subsystems/climber.py
@@ -18,6 +18,8 @@ class Climber(SafeSubsystem):
     stall_limit = autoproperty(15)
     free_limit = autoproperty(30)
     sim_max_height = 100
+    height_max = autoproperty(100.0)
+    height_min = autoproperty(0.0)
 
     def __init__(self, port_motor, port_switch_up, port_switch_down):
         super().__init__()
@@ -25,9 +27,13 @@ class Climber(SafeSubsystem):
         configureLeader(
             self._motor, "brake", stallLimit=self.stall_limit, freeLimit=self.free_limit
         )
+        self._encoder = self._motor.getEncoder()
 
         self._switch_up = Switch(port_switch_up, Switch.Type.NormallyOpen)
         self._switch_down = Switch(port_switch_down, Switch.Type.NormallyOpen)
+
+        self._prev_is_up = False
+        self._offset = 0.0
 
         if RobotBase.isSimulation():
             self._sim_motor = SparkMaxSim(self._motor)
@@ -48,10 +54,21 @@ class Climber(SafeSubsystem):
         self._motor.set(0)
 
     def isUp(self):
-        return self._switch_up.isPressed()
+
+        
+        return self._switch_up.isPressed() or self._encoder.getPosition() >= self.height_max
 
     def isDown(self):
-        return self._switch_down.isPressed()
+        return self._switch_down.isPressed() or self._encoder.getPosition() <= self.height_min
+
+    def periodic(self) -> None:
+        if self._prev_is_up and not self._switch_up.isPressed():
+            self._offset = self.height_max - self._encoder.getPosition()
+
+        self._prev_is_up = self._switch_up.isPressed()
+
+    def getHeight(self):
+        return self._encoder.getPosition() + self._offset
 
     def simulationPeriodic(self) -> None:
         self._sim_motor.setVelocity(self._motor.get())
