95616488abb51fc8e237259380caef87cee5e921
Thomas McDonald <4001137@keranna.qc.ca>
2024-02-07 19:30:33 -0500

    Added last_vr and ran black

--- commands/vision/alignwithtag2d.py
+++ commands/vision/alignwithtag2d.py
@@ -21,22 +21,13 @@ class AlignWithTag2D(SafeCommand):
     align_threshold = autoproperty(0.1)
 
     def __init__(self, drivetrain: Drivetrain, tag_id: int):
+        super().__init__()
         self.drivetrain = drivetrain
         self.addRequirements(drivetrain)
         self.has_tag = False
         self.tag_id = tag_id
-        self.vr: float = None
-
-    # def initialize(self):
-    #     results = self.drivetrain.cam.getLatestResult().getTargets()
-    #     results = results.sort(key=lambda x: abs(x.getYaw()))
-    #     if len(results) is not 0:
-    #         nearest_tag: PhotonTrackedTarget = results[0]
-    #         self.tag_id = nearest_t
-    #         ag.getFiducialId()
-    #         self.has_tag = True
-    #     else:
-    #         self.has_tag = False
+        self.vr: Optional[float] = None
+        self.last_vr: Optional[float] = None
 
     def execute(self):
         results = self.drivetrain.cam.getLatestResult().getTargets()
@@ -45,7 +36,10 @@ class AlignWithTag2D(SafeCommand):
             self.has_tag = True
             self.vr = min(abs(target.getYaw()), self.align_speed)
             self.vr = math.copysign(self.vr, target.getYaw() * -1)
-            self.drivetrain.drive(0, 0, self.vr)
+            self.last_vr = self.vr
+            self.drivetrain.drive(0, 0, self.vr, True)
+        elif self.last_vr is not None:
+            self.drivetrain.drive(0, 0, self.last_vr, True)
         else:
             self.has_tag = False
 
