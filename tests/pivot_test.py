ee2ab03528d9a2636e04d2e70261dd79885aaeb8
Étienne Thibault <4001491@keranna.qc.ca>
2024-02-11 14:49:50 -0500

    Deleted big useless chunk of comment in pivot

--- tests/pivot_test.py
+++ tests/pivot_test.py
@@ -6,39 +6,6 @@ from commands.pivot.resetpivotdown import ResetPivotDown
 from robot import Robot
 
 
-#def test_resetCommand(control, robot: Robot):
-    #with control.run_robot():
-        # Enable robot and schedule command
-        #control.step_timing(seconds=0.1, autonomous=False, enabled=True)
-        #cmd = ResetPivot(robot.pivot)
-       # cmd.schedule()
-        #control.step_timing(seconds=0.1, autonomous=False, enabled=True)
-
-        # Set switch_up and switch_down to "unpressed" and check speed_up
-        #robot.pivot._sim_encoder.setCount(150)  # Set encoder to the middle so no switch is pressed
-        #control.step_timing(seconds=0.1, autonomous=False, enabled=True)
-        #assert robot.pivot._motor.get() <= 0.001
-
-        # Set switch_up to "pressed" and check speed_down
-        #np = robot.pivot._switch_up.isPressed()
-        #robot.pivot._sim_encoder.setCount(255)  # Set encoder to the maximum value so switch_up is pressed
-        #control.step_timing(seconds=0.1, autonomous=False, enabled=True)
-        #c = robot.pivot._sim_encoder.getCount()
-        #v = robot.pivot._sim_motor.getSpeed()
-       # p = robot.pivot._switch_up.isPressed()
-        #assert robot.pivot._motor.get() <= 0.001
-
-        # Set switch_up to "unpressed" and check if motor stops
-        #robot.pivot._sim_encoder.setCount(150)  # Set encoder to the middle so no switch is pressed
-       # control.step_timing(seconds=3.0, autonomous=False, enabled=True)
-       # assert 0.0 == approx(robot.pivot._motor.get(), rel=0.01)
-
-        # Check if encoder has been set to 0 and if command has stopped
-       # control.step_timing(seconds=0.1, autonomous=False, enabled=True)
-       # assert 0.0 == approx(robot.pivot._encoder.get())
-       # assert not cmd.isScheduled()
-
-
 def test_movePivot_from_swich_down(control, robot: Robot):
     with control.run_robot():
 
