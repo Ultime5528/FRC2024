cea983b55e6232b72b08ae531d8dda07911ad6d3
Étienne Beaulac <etienne.beaulac@outlook.com>
2024-03-13 14:16:38 -0400

    Removed controller from PickUp

--- robot.py
+++ robot.py
@@ -203,7 +203,7 @@ class Robot(commands2.TimedCommandRobot):
         # Copilot's panel
         AxisTrigger(self.panel_1, 1, "down").whileTrue(ExtendClimber(self.climber_left))
         AxisTrigger(self.panel_1, 1, "up").whileTrue(RetractClimber(self.climber_left))
-        self.panel_1.button(3).onTrue(PickUp(self.intake, self.xbox_controller))
+        self.panel_1.button(3).onTrue(PickUp(self.intake))
         self.panel_1.button(2).onTrue(Drop(self.intake))
         self.panel_1.button(1).onTrue(MovePivot.toSpeakerClose(self.pivot))
 
@@ -293,7 +293,7 @@ class Robot(commands2.TimedCommandRobot):
             )
 
         putCommandOnDashboard("Intake", Drop(self.intake))
-        putCommandOnDashboard("Intake", PickUp(self.intake, self.xbox_controller))
+        putCommandOnDashboard("Intake", PickUp(self.intake))
         putCommandOnDashboard("Intake", Load(self.intake))
 
         putCommandOnDashboard("Pivot", MovePivot.toAmp(self.pivot))
